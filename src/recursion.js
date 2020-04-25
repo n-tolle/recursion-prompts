d2e90a2cf1256a2fa205e15892ad0682b55db97f
Nicholas <nbtolle@gmail.com>
2020-04-25 16:31:16 -0600

    Solve reverse

--- src/recursion.js
+++ src/recursion.js
@@ -121,6 +121,11 @@ var powerOfTwo = function(n) {
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+  if (string.length === 0){
+    return '';
+  } else {
+    return string[string.length - 1] + reverse(string.substring(0, string.length - 1));
+  }
 };
 
 // 10. Write a function that determines if a string is a palindrome.
