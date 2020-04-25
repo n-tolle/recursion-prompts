9a4761eb9da2ec07d6605fb6f804c199e39372a2
Nicholas <nbtolle@gmail.com>
2020-04-25 15:23:35 -0600

    Solve range

--- src/recursion.js
+++ src/recursion.js
@@ -75,6 +75,17 @@ var sumBelow = function(n) {
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+  if (x === (y - 1) || x === y || (x - 1) === y){
+    return [];
+  } else if (x < y) {
+    let nums = [];
+    nums.push(x + 1);
+    return nums.concat(range(x + 1, y));
+  } else {
+    let nums = [];
+    nums.push(x - 1);
+    return nums.concat(range(x - 1, y));
+  }
 };
 
 // 7. Compute the exponent of a number.
