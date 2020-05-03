406b244e492a81b8845c033cc55f6323098538e4
Nicholas <nbtolle@gmail.com>
2020-05-03 09:16:52 -0600

    Fix multiply in recursion.js The multiply function now returns the correct product of 2 numbers

--- src/recursion.js
+++ src/recursion.js
@@ -185,6 +185,13 @@ var modulo = function(x, y) {
 // 12. Write a function that multiplies two numbers without using the * operator or
 // Math methods.
 var multiply = function(x, y) {
+  if (x === 0 || y === 0){
+    return 0;
+  } else if ((x < 0 && y < 0) || (x > 0 && y < 0)){
+    x = -x;
+    y = -y;
+  }
+  return x + multiply(x, y - 1);
 };
 
 // 13. Write a function that divides two numbers without using the / operator or
