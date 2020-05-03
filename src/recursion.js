cbb2669e9e5cd9b6fe4a8dc425e8eb4b7ba04b92
Nicholas <nbtolle@gmail.com>
2020-05-03 15:29:55 -0600

    Fix countOccurrence in recursion.js countOccurrence now takes an array and a value and returns the number of times the value appears in the array.

--- src/recursion.js
+++ src/recursion.js
@@ -328,6 +328,15 @@ var fizzBuzz = function(n) {
 // countOccurrence([2,7,4,4,1,4], 4) // 3
 // countOccurrence([2,'banana',4,4,1,'banana'], 'banana') // 2
 var countOccurrence = function(array, value) {
+  let count = 0;
+  if (array.length === 0){
+    return count;
+  } else {
+    if (array[0] === value){
+      count++;
+    }
+  }
+  return count + countOccurrence(array.slice(1), value);
 };
 
 // 21. Write a recursive version of map.
