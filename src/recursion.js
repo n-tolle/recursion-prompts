0ab957d2effd49c0ad8d96eec87d107972734638
Nicholas <nbtolle@gmail.com>
2020-05-03 09:54:16 -0600

    Fix divide in recursion.js The divide function will now return the correct results (excluding decimals) when given 2 numbers It will also return NaN when the given denominator is 0 and 0 when the given numerator is 0

--- src/recursion.js
+++ src/recursion.js
@@ -197,6 +197,38 @@ var multiply = function(x, y) {
 // 13. Write a function that divides two numbers without using the / operator or
 // Math methods to arrive at an approximate quotient (ignore decimal endings).
 var divide = function(x, y) {
+  if ((x < 0 && y < 0) || (x > 0 && y < 0)){
+    x = -x;
+    y = -y;
+  }
+  if (y === 0){
+    return NaN;
+  }
+  if (x === 0){
+    return 0;
+  }
+  let count = 0;
+  if (x < 0){
+    if (x + y > 0){
+      return 0;
+    } else if (x + y === 0){
+      count--;
+      return count;
+    } else {
+      count--;
+      return count + divide(x + y, y);
+    }
+  } else {
+    if (x - y < 0){
+      return 0;
+    } else if (x - y === 0){
+      count++;
+      return count;
+    } else {
+      count++;
+      return count + divide(x - y, y);
+    }
+  }
 };
 
 // 14. Find the greatest common divisor (gcd) of two positive numbers. The GCD of two
