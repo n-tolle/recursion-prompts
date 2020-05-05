aa23df48c8992ff8e251d767c0bad1d7022c36b4
Nicholas <nbtolle@gmail.com>
2020-05-04 18:13:56 -0600

    Fix fibonnaci in recursion.js fibonnaci will now return an array containing a Fibonnaci sequence up to the nth index.

--- src/recursion.js
+++ src/recursion.js
@@ -405,6 +405,17 @@ var replaceKeysInObj = function(obj, oldKey, newKey) {
 // fibonacci(5); // [0,1,1,2,3,5]
 // Note: The 0 is not counted.
 var fibonacci = function(n) {
+  let startArr = [0, 1];
+  if (n < 1){
+    return null;
+  } else if (n === 1){
+    return startArr;
+  } else {
+    for (let i = 2; i < n; i++){
+      startArr.push(startArr[i - 1] + startArr[i - 2]);
+    }
+    return fibonacci(n - 1).concat([startArr[n - 1] + startArr[n - 2]]);
+  }
 };
 
 // 26. Return the Fibonacci number located at index n of the Fibonacci sequence.
