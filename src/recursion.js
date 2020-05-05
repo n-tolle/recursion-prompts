0f9ebc1e12f34a83dcf96a7143881da93c4443b9
Nicholas <nbtolle@gmail.com>
2020-05-04 19:07:36 -0600

    Fix nthFibo in recursion.js nthFibo will now return the number located at the given index in a Fibonacci sequence. Looked up the math on this one for how to find the nth number in a Fibonnaci sequence and found an explanation showing that any number in a Fibonnaci sequence after the 0 and 1 can be found using the equation Fibonnaci(n) = Fibonnaci(n-1) + Fibonnaci(n-2).

--- src/recursion.js
+++ src/recursion.js
@@ -424,6 +424,14 @@ var fibonacci = function(n) {
 // nthFibo(7); // 13
 // nthFibo(3); // 2
 var nthFibo = function(n) {
+  let count = 0;
+  if (n < 0){
+    return null;
+  } else if (n <= 1){
+    return n;
+  } else {
+    return nthFibo(n - 1) + nthFibo(n - 2);
+  }
 };
 
 // 27. Given an array of words, return a new array containing each word capitalized.
