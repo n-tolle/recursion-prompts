58702ad1f587b3afa23278c06d32b2a867d00af8
Nicholas <nbtolle@gmail.com>
2020-05-06 17:52:20 -0600

    Fix flatten in recursion.js flatten now returns a flat array containing all elements of a nested array.

--- src/recursion.js
+++ src/recursion.js
@@ -483,6 +483,15 @@ var nestedEvenSum = function(obj) {
 // 30. Flatten an array containing nested arrays.
 // flatten([1,[2],[3,[[4]]],5]); // [1,2,3,4,5]
 var flatten = function(array) {
+  let flatArr = [];
+  for (let i = 0; i < array.length; i++){
+    if (Array.isArray(array[i])){
+      flatArr = flatArr.concat(flatten(array[i]));
+    } else {
+      flatArr.push(array[i]);
+    }
+  }
+  return flatArr;
 };
 
 // 31. Given a string, return an object containing tallies of each letter.
