9e9d77a5011fd1eeb0208a993483568b113bfbe9
Nicholas <nbtolle@gmail.com>
2020-05-06 18:00:15 -0600

    Fix letterTally in recursion.js letterTally will now return an object where each character in the given string is a key and the value of each is the number of times that character appears in the given string.

--- src/recursion.js
+++ src/recursion.js
@@ -496,7 +496,16 @@ var flatten = function(array) {
 
 // 31. Given a string, return an object containing tallies of each letter.
 // letterTally('potato'); // {p:1, o:2, t:2, a:1}
-var letterTally = function(str, obj) {
+var letterTally = function(str, obj = {}) {
+  if (str.length === 0){
+    return obj;
+  } else if (obj[str[0]] === undefined){
+    obj[str[0]] = 1;
+    return letterTally(str.substring(1), obj);
+  } else {
+    obj[str[0]]++;
+    return letterTally(str.substring(1), obj);
+  }
 };
 
 // 32. Eliminate consecutive duplicates in a list. If the list contains repeated
